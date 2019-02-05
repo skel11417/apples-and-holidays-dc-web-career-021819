c2cdfb3cfd6030ca9831b1aadd48dc02d4c82aa5
Sean Kelly <shonkelli@gmail.com>
2019-02-05 18:00:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
       supply_str = supplies.join(", ")
-      puts "  #{sym_format(holiday_name)}: "
+      puts "  #{sym_format(holiday_name)}: #{supply_str}"
   end
 end
 
