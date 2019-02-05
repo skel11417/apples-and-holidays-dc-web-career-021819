c3cec1438baefed286b2a6c24562bdcb07d73254
Sean Kelly <shonkelli@gmail.com>
2019-02-05 17:56:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,8 @@ def all_winter_holiday_supplies(holiday_hash)
   supplies.flatten
 end
 
-def cool_caps(name)
+def sym_format(name)
+  
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
