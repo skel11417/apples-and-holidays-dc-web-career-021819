61c63fef959ef11a3be51fc451a1bc897bf31962
Sean Kelly <shonkelli@gmail.com>
2019-02-05 17:58:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def sym_format(sym)
   str = sym.to_s
   str_a = str.split(" ")
   new_a = str_a.map {|x| x.capitalize}
-  
+  output = new_a.join(" ")
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
