ed1eac134f994906e91e47033aece228338486e0
Sean Kelly <shonkelli@gmail.com>
2019-02-05 17:59:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
-      
+      puts "  #{sym_format(holiday_name)}: "
   end
 end
 
